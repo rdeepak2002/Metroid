be7ffeddbd4528d0bfeacf2888b71d783edcc76e
Deepak Ramalingam <deepak@deepak-K55A.(none)>
2014-04-02 06:46:36 -0600

    music and boss

--- metroid.py
+++ metroid.py
@@ -11,6 +11,10 @@ leftfacing = "img/metroidleftfacing.png"
 leftrunning = "img/metroidleftrunning1.png"
 leftrunning2 = "img/metroidleftrunning2.png"
 blast_img = "img/blast.png"
+metroidnormal = "img/metroidnormal.png"
+metroidattacking1 = "img/metroidattacking1.png"
+metroidattacking2 = "img/metroidattacking2.png"
+metroidattacking3 = "img/metroidattacking3.png"
 
 # housekeeping
 pygame.init()
@@ -18,8 +22,12 @@ screen = pygame.display.set_mode((800,500),0,32)
 background=pygame.image.load(bif).convert()
 
 # state:[jump,direction,motion]
-samus = {"x":400,"y":360,"hp":100,"ammo":100,"dx":0,"dy":0,"current_img":mif,"state":[0,0,0]}
+samus = {"x":400,"y":360,"hp":100,"ammo":1000,"dx":0,"dy":0,"current_img":mif,"state":[0,0,0]}
+metroid = {"x": 600, "y":280, "hp":1000, "current_img":metroidnormal, "met_state":0}
 blasts = []
+
+pygame.mixer.music.load('boss.ogg')
+pygame.mixer.music.play(-1)
 
 clock=pygame.time.Clock()
 speed=150
@@ -123,6 +131,9 @@ while True:
 	for blast in blasts:
 		blast['x'] += blast['dx']
 
+		if blast ['x']>830:
+			blasts.remove(blast)
+
 	if samus['state'][1] == 0 and samus['state'][2] == 1:
 		if digit % 2 == 0:
 			samus['current_img'] = leftrunning
@@ -135,20 +146,41 @@ while True:
 		else:
 			samus['current_img'] = rightrunning2
 
+	#too close to metroid
+	if samus['x']>metroid['x']:
+		metroid["met_state"] = 1
+		samus['hp'] -= 40
+		samus ['dx'] = -5
+
+	if metroid['met_state'] == 1:
+		if metroid['met_state'] ==1:
+			#add timer
+			pygame.time.Clock.tick(framerate=0)
+			pygame.time.Clock.get_time()
+			metroid['current_img'] = metroidattacking2
+			metroid['current_img'] = metroidattacking3
+			#add time
+			metroid['current_img'] = metroidnormal
+			metroid["met_state"] == 0
+
 	font = pygame.font.Font(None, 30)
-	textImg = font.render("HEALTH: " + str(samus['hp']) + "       Ammo: " + str(samus['ammo']), 1, (255, 0, 0))
+	textImg = font.render("SAMUS HEALTH: " + str(samus['hp']) + "                   Ammo: " + str(samus['ammo']) + "                   METROID HEALTH: " + str(metroid['hp']), 1, (255, 0, 0))
 
 	# update current state of sprite
 	sprite = pygame.image.load(samus['current_img']).convert_alpha()
+	metroid_sprite = pygame.image.load(metroid['current_img']).convert_alpha()
 	blast_sprite=pygame.image.load(blast_img).convert_alpha()
 
 	# artsy stuff
 	screen.blit (background, (0,0))
 	screen.blit (sprite, (samus['x'],samus['y']))
+	screen.blit (metroid_sprite, (metroid ['x'], metroid ['y']))
+
 	for blast in blasts:
 		if (blast['x'] <= 800 or blast['x'] >= -100):
 			screen.blit (blast_sprite, (blast['x'],blast['y']))
+
 	screen.blit(textImg,(0,0))
 
 	# update display
-	pygame.display.update()
+	pygame.display.flip()
